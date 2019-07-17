de795d6ca7fff7c4efbc078bc11da73ff8bf519d
Randall Taylor <randyisbusy@gmail.com>
2019-07-17 13:06:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(sec)
-  sec = count
-  until count == 0 do
-    puts "#{count} SECOND(S)!"
-    count -= 1
+  
+  until sec == 0 do
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
   end
   x = "HAPPY NEW YEAR!"
   x
