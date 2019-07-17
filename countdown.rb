722f34410723a9c67125a7f17285b4f9922344c3
Randall Taylor <randyisbusy@gmail.com>
2019-07-17 13:17:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+def countdown(sec)
+  until sec === 0 do
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+  end
+  x = "HAPPY NEW YEAR!"
+   x
+end
 
-def countdown
+def countdown_with_sleep(sec)
+
+  until sec == 0 do
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+    sleep(1)
+  end
+  x = "HAPPY NEW YEAR!"
+  puts x
 end
